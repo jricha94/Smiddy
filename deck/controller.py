9a847d5e10b9d9dd7a4f4b52ebb8f42c3942dfd9
Jarod Richardson <jricha94@vols.utk.edu>
2021-04-05 13:01:13 -0400

    Small change

--- deck/controller.py
+++ deck/controller.py
@@ -3,26 +3,34 @@ from burn import burn
 
 results = open('results.txt','w')
 
-test = burn('thorConSalt', 'thorConSalt')
-test.iterate_rho()
+test = burn(salt='thorConSalt', rep_salt='thorConSalt')
+test.iterate_rho() #gets critical enrichment
 print('good')
 # 2 methods below arent important, just to store data
-test.plot_iters()
-test.save_iters()
+test.plot_iters() #plots criticality search
+test.save_iters() # saves criticality search
 
 
 results.write('crit e: ' + str(test.conv_enr)+'\n\n')
-test.e0 = test.conv_enr
 
 
-test.rep_e = 19.75
-test.get_rep_rate(True)
+test.e0 = test.conv_enr     # change enrichment to one found in crit seach 
+test.rep_e = 19.75          # set repr enrichment
+
+test.get_rep_rate(True)     # finds the reprocessing rate
 
 print('good')
 
 results.write('rep rate:' + str(test.rep_rate)+'\n\n')
-test.run_feedbacks(True, False)
-test.read_feedbacks()
+
+#Options:
+#fs.dopp   =    fuel salt doppler 
+#gr.dopp   =    graphite doppler
+#both      =    both dopplers
+
+
+test.run_feedbacks(feedback='fs.dopp', recalc=True)     # Runs decks doppler feedback
+test.read_feedbacks(feedback='fs.dopp')                # Reads results from doppler feedback
 
 print('done')
 
