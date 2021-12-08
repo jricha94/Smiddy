8ad68e3dcc191858e568f7dd67535ceeaa1a1545
Jarod Richardson <jricha94@vols.utk.edu>
2021-12-08 13:22:21 -0500

    results

--- NE571/project6/project6.py
+++ NE571/project6/project6.py
@@ -92,7 +92,7 @@ class core:
             #PBS -q fill
             #PBS -l nodes=1:ppn=8
 
-            cd ${{PBS_0_WOKDIR}}
+            cd ${{PBS_O_WORKDIR}}
 
             module load studsvik
             casmo4 {self.input}.inp
--- NE571/project6/pwr/run.sh
+++ NE571/project6/pwr/run.sh
@@ -4,7 +4,7 @@
 #PBS -q fill
 #PBS -l nodes=1:ppn=8
 
-cd ${PBS_0_WOKDIR}
+cd ${PBS_O_WORKDIR}
 
 module load studsvik
 casmo4 pwr.inp
